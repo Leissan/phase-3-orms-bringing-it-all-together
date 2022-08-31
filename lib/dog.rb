30320c8b61280beb0709bdcb78e6356ba631afb5
Leissan Khan <leissan.khan@gmail.com>
2022-08-30 23:19:47 -0400

    commit tree 7f7ea291615fa287291cb1ecd2c5cff9c7019eec

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,72 @@
 class Dog
 
-end
+    attr_accessor :name, :breed, :id
+  
+    def initialize(name:, breed:, id: nil)
+      @id = id
+      @name = name
+      @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+          CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+          )
+        SQL
+    
+        DB[:conn].execute(sql)
+      end
+
+      def self.drop_table
+        sql = <<-SQL
+          DROP TABLE IF EXISTS dogs
+        SQL
+    
+        DB[:conn].execute(sql)
+      end
+
+      def save
+        sql = <<-SQL
+          INSERT INTO dogs (name, breed)
+          VALUES (?, ?)
+        SQL
+    
+        # insert the dogs
+        DB[:conn].execute(sql, self.name, self.breed)
+    
+        # get the song ID from the database and save it to the Ruby instance
+        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    
+        # return the Ruby instance
+        self
+
+      end
+
+      def self.create(name:, breed:)
+        dogs = Song.new(name: name, breed: breed)
+        dogs.save
+      end
+
+      def self.new_from_db(row)
+        # self.new is equivalent to Song.new
+        self.new(id: row[0], name: row[1], breed: row[2])
+    
+      end
+
+      def self.all
+        sql = <<-SQL
+          SELECT *
+          FROM dogs
+        SQL
+    
+        DB[:conn].execute(sql).map do |row|
+          self.new_from_db(row)
+        end
+      end
+
+
+  
+  end
